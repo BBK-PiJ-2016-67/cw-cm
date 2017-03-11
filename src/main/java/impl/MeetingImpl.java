341e082df6ae1bd3f7e638d5fb06215519a41d60
lucas <lucasjgordon@gmail.com>
2017-03-11 17:54:39 +0000

    Uncomments default meeting tests, adds mockcontactimpl and adds base meetingimpl class.

---
+++ src/main/java/impl/MeetingImpl.java
@@ -0,0 +1,12 @@
+package impl;
+
+import spec.Meeting;
+
+/**
+ * MeetingImpl is a class implementing {@see Meeting}.
+ *
+ * @author BBK-PiJ-2016-67
+ */
+public abstract class MeetingImpl implements Meeting {
+
+}
---
+++ src/main/java/impl/MockContactImpl.java
@@ -0,0 +1,28 @@
+package impl;
+
+import spec.Contact;
+
+/**
+ * MockContactImpl is a mock class implementing {@see Contact}.
+ *
+ * @author BBK-PiJ-2016-67
+ */
+public class MockContactImpl implements Contact {
+    public MockContactImpl(int ID, String name) {}
+
+    public MockContactImpl(int ID, String name, String notes) {}
+
+    public int getId() {
+        return 1;
+    }
+
+    public String getName() {
+        return "joe";
+    }
+
+    public String getNotes() {
+        return "good meeting";
+    }
+
+    public void addNotes(String note) {}
+}
