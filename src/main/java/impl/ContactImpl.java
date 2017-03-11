093409bbfd02e836a0e2bd05663cfdc10ea6b6c2
lucas <lucasjgordon@gmail.com>
2017-03-11 16:47:59 +0000

    Adds empty methods to ContactImpl to conform to interface.

--- src/main/java/impl/ContactImpl.java
+++ src/main/java/impl/ContactImpl.java
@@ -8,5 +8,41 @@ import spec.Contact;
  * @author BBK-PiJ-2016-67
  */
 public class ContactImpl implements Contact {
+    // TODO - two constructors
+    /** The implementation of this interface must have two constructors. The most general constructor
+        must have three parameters: int, String, String. The first one corresponds to the ID
+        provided by the ContactManager, the next one corresponds to the name, and the last one
+        corresponds to the initial set of notes about the contact. Another, more restricted constructor
+        must have two parameters only: ID and name. If the ID provided is zero or negative, a
+        IllegalArgumentException must be thrown. If any of the references / pointers passed as
+        parameters to the constructor is null, a NullPointerException must be thrown.
+    */
 
+    /**
+     * {@inheritDoc}.
+     */
+    public int getId() {
+        return 0;
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    public String getName() {
+        return "";
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    public String getNotes() {
+        return "";
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    public void addNotes(String note) {
+
+    }
 }
