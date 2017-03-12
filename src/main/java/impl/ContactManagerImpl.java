8cfcff23dab316f5d427cfd59fbb5271383fedf5
lucas <lucasjgordon@gmail.com>
2017-03-12 18:36:45 +0000

    Creates basic contactmanagerimpl class.

---
+++ src/main/java/impl/ContactManagerImpl.java
@@ -0,0 +1,12 @@
+package impl;
+
+import spec.ContactManager;
+
+/**
+ * ContactManagerImpl is a class implementing {@see ContactManager}.
+ *
+ * @author BBK-PiJ-2016-67
+ */
+public final class ContactManagerImpl implements ContactManager {
+
+}
