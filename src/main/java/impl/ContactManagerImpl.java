031c5182a7be410e65825c0a9e18d5be5e82e49b
lucas <lucasjgordon@gmail.com>
2017-03-12 18:39:02 +0000

    Implements contactmanager interface methods.

--- src/main/java/impl/ContactManagerImpl.java
+++ src/main/java/impl/ContactManagerImpl.java
@@ -8,5 +8,94 @@ import spec.ContactManager;
  * @author BBK-PiJ-2016-67
  */
 public final class ContactManagerImpl implements ContactManager {
+    /**
+     * {@inheritDoc}.
+     */
+    public int addFutureMeeting(Set<Contact> contacts, Calendar date) {
+        
+    }
 
+    /**
+     * {@inheritDoc}.
+     */
+    public PastMeeting getPastMeeting(int id) {
+        
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    public FutureMeeting getFutureMeeting(int id) {
+        
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    public Meeting getMeeting(int id) {
+        
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    public List<Meeting> getFutureMeetingList(Contact contact) {
+        
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    public List<Meeting> getMeetingListOn(Calendar date) {
+        
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    public List<PastMeeting> getPastMeetingListFor(Contact contact) {
+        
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    public int addNewPastMeeting(Set<Contact> contacts, Calendar date, String text) {
+        
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    public PastMeeting addMeetingNotes(int id, String text) {
+        
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    public int addNewContact(String name, String notes) {
+        
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    public Set<Contact> getContacts(String name) {
+        
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    public Set<Contact> getContacts(int... ids) {
+        
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    public void flush() {
+        
+    }
 }
