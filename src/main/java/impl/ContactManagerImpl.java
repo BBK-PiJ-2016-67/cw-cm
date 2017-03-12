894effcc67b4664c94dfe13eac2b9333940f6843
lucas <lucasjgordon@gmail.com>
2017-03-12 18:47:21 +0000

    Imports required libraries to ContactManagerImpl and adds default return values.

--- src/main/java/impl/ContactManagerImpl.java
+++ src/main/java/impl/ContactManagerImpl.java
@@ -1,6 +1,16 @@
 package impl;
 
+import spec.Contact;
 import spec.ContactManager;
+import spec.FutureMeeting;
+import spec.Meeting;
+import spec.PastMeeting;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * ContactManagerImpl is a class implementing {@see ContactManager}.
@@ -12,84 +22,84 @@ public final class ContactManagerImpl implements ContactManager {
      * {@inheritDoc}.
      */
     public int addFutureMeeting(Set<Contact> contacts, Calendar date) {
-        
+        return 1;
     }
 
     /**
      * {@inheritDoc}.
      */
     public PastMeeting getPastMeeting(int id) {
-        
+        return new PastMeetingImpl(1, Calendar.getInstance(), new HashSet<Contact>(), "");
     }
 
     /**
      * {@inheritDoc}.
      */
     public FutureMeeting getFutureMeeting(int id) {
-        
+        return new FutureMeetingImpl(1, Calendar.getInstance(), new HashSet<Contact>());
     }
 
     /**
      * {@inheritDoc}.
      */
     public Meeting getMeeting(int id) {
-        
+        return new FutureMeetingImpl(1, Calendar.getInstance(), new HashSet<Contact>());
     }
 
     /**
      * {@inheritDoc}.
      */
     public List<Meeting> getFutureMeetingList(Contact contact) {
-        
+        return new ArrayList<Meeting>();
     }
 
     /**
      * {@inheritDoc}.
      */
     public List<Meeting> getMeetingListOn(Calendar date) {
-        
+        return new ArrayList<Meeting>();
     }
 
     /**
      * {@inheritDoc}.
      */
     public List<PastMeeting> getPastMeetingListFor(Contact contact) {
-        
+        return new ArrayList<PastMeeting>();
     }
 
     /**
      * {@inheritDoc}.
      */
     public int addNewPastMeeting(Set<Contact> contacts, Calendar date, String text) {
-        
+        return 1;
     }
 
     /**
      * {@inheritDoc}.
      */
     public PastMeeting addMeetingNotes(int id, String text) {
-        
+        return new PastMeetingImpl(1, Calendar.getInstance(), new HashSet<Contact>(), "");
     }
 
     /**
      * {@inheritDoc}.
      */
     public int addNewContact(String name, String notes) {
-        
+        return 1;
     }
 
     /**
      * {@inheritDoc}.
      */
     public Set<Contact> getContacts(String name) {
-        
+        return new HashSet<Contact>();
     }
 
     /**
      * {@inheritDoc}.
      */
     public Set<Contact> getContacts(int... ids) {
-        
+        return new HashSet<Contact>();
     }
 
     /**
