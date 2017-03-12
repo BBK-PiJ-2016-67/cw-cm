d1f85c3107e894bcc6f68867f00ac9ac0fe922be
lucas <lucasjgordon@gmail.com>
2017-03-12 18:05:57 +0000

    Uncomments pastmeeting tests, creates basic pastmeetingimpl class.

---
+++ src/main/java/impl/PastMeetingImpl.java
@@ -0,0 +1,17 @@
+package impl;
+
+import spec.Contact;
+import spec.PastMeeting;
+
+import java.util.Calendar;
+import java.util.HashSet;
+
+/**
+ * PastMeetingImpl is a class implementing {@see PastMeeting}
+ * and extending {@see MeetingImpl}.
+ *
+ * @author BBK-PiJ-2016-67
+ */
+public final class PastMeetingImpl extends MeetingImpl implements PastMeeting {
+
+}
--- src/test/java/test/PastMeetingTest.java
+++ src/test/java/test/PastMeetingTest.java
@@ -1,54 +1,53 @@
-// package test;
+package test;
 
-// import impl.MeetingImpl;
-// import impl.MockContactImpl;
-// import org.junit.Before;
-// import org.junit.Test;
-// import spec.Contact;
-// import spec.PastMeeting;
+import impl.PastMeetingImpl;
+import impl.MockContactImpl;
+import org.junit.Before;
+import org.junit.Test;
+import spec.Contact;
+import spec.Meeting;
 
-// import java.util.Calendar;
-// import java.util.HashSet;
-// import java.util.Set;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
 
-// import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
-// public class PastMeetingTest {
+public class PastMeetingTest {
 
-//     Calendar pastDate;
-//     Set<Contact> contacts;
-//     PastMeeting pastMeeting;
+    Calendar pastDate;
+    Set<Contact> contacts;
+    Meeting pastMeeting;
 
-//     @Before
-//     public void setUp() {
-//         pastDate = Calendar.getInstance();
-//         pastDate.add(Calendar.YEAR, -1);
-//         contacts = new HashSet<Contact>();
-//         contacts.add(new MockContactImpl());
-//         contacts.add(new MockContactImpl());
-//         pastMeeting = new MeetingImpl(pastDate, contacts);
-//     }
+    @Before
+    public void setUp() {
+        pastDate = Calendar.getInstance();
+        pastDate.add(Calendar.YEAR, -1);
+        contacts = new HashSet<Contact>();
+        contacts.add(new MockContactImpl(1, "", ""));
+        contacts.add(new MockContactImpl(1, "", ""));
+        pastMeeting = new PastMeetingImpl(pastDate, contacts);
+    }
 
-//     @Test
-//     public void testGetNotesNone() {
-//         assertEquals("", pastMeeting.getNotes());
-//     }
+    @Test
+    public void testGetNotesNone() {
+        assertEquals("", pastMeeting.getNotes());
+    }
 
-//  /*
-//     @Test
-//     public void testAddNotesSingle() {
-//         pastMeeting.addNotes("Notes 1");
+    @Test
+    public void testAddNotesSingle() {
+        pastMeeting.addNotes("Notes 1");
 
-//         assertTrue(pastMeeting.getNotes().contains("Notes 1"));
-//     }
+        assertTrue(pastMeeting.getNotes().contains("Notes 1"));
+    }
 
-//     @Test
-//     public void testAddNotesMultiple() {
-//         pastMeeting.addNotes("Notes 1");
-//         pastMeeting.addNotes("Notes 2");
+    @Test
+    public void testAddNotesMultiple() {
+        pastMeeting.addNotes("Notes 1");
+        pastMeeting.addNotes("Notes 2");
 
-//         assertTrue(pastMeeting.getNotes().contains("Notes 1"));
-//         assertTrue(pastMeeting.getNotes().contains("Notes 2"));
-//     }
-//     */
-// }
+        assertTrue(pastMeeting.getNotes().contains("Notes 1"));
+        assertTrue(pastMeeting.getNotes().contains("Notes 2"));
+    }
+
+}
